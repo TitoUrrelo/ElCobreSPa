6444ee7f845e6288ae34e996f15532d7beb319d4
Tito Henrry Urrelo Aquino <tito.urrelo@inacapmail.cl>
2025-11-19 23:02:37 -0300

    XD

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -8,15 +8,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDyvGTl21W5eQu1GUfre9MGiE23Gm_S8m0",
-  authDomain: "lavanderiaelcobre-4212b.firebaseapp.com",
-  projectId: "lavanderiaelcobre-4212b",
-  storageBucket: "lavanderiaelcobre-4212b.firebasestorage.app",
-  messagingSenderId: "250289358691",
-  appId: "1:250289358691:web:05d026af7a59ecb98f3556",
-  measurementId: "G-S8HV70WF2M"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);

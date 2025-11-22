6c555b39764c856f802df836ff3a3dc3f4cdd5e3
Tito Henrry Urrelo Aquino <tito.urrelo@inacapmail.cl>
2025-11-22 06:22:58 -0300

    eliminacion de inicioSesion

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import LoginScreen from './screens/LoginScreen';
 import RecepcionistaHomeScreen from './screens/RecepcionistaHomeScreen';
 import RegistrarClienteScreen from './screens/RegistrarClienteScreen';
 import CrearComandaScreen from './screens/CrearComandaScreen';
@@ -16,8 +15,7 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Navigator initialRouteName="AdministradorHome" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="RecepcionistaHome" component={RecepcionistaHomeScreen} />
         <Stack.Screen name="AdministradorHome" component={AdministradorHomeScreen} />
         <Stack.Screen name="RegistrarCliente" component={RegistrarClienteScreen} />
